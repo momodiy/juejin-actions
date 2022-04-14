d64f3eb06e948aeb99d9f77c292dc3cecabf51b0
StevenLee <qcstevengo@gmail.com>
2022-04-14 18:14:30 +0800

    ：sparkles： throttle debounce ...

--- src/services/index.js
+++ src/services/index.js
@@ -68,3 +68,4 @@ export const freeDraw = async function(){
         method: 'post'
     });
 }
+
--- test/env.js
+++ test/env.js
@@ -1,17 +1,17 @@
 // 此文件存放所有环境变量
 
 // 掘金cookie
-export const JUEJIN_COOKIE = '';
+export const JUEJIN_COOKIE = '_ga=GA1.2.1906172502.1638947757; MONITOR_WEB_ID=abb1eaac-94e4-432f-b64a-ca3e96417818; _tea_utm_cache_2608=undefined; __tea_cookie_tokens_2608=%257B%2522web_id%2522%253A%25227039226993380476457%2522%252C%2522ssid%2522%253A%25222ed1ca60-b655-4963-aad4-6257b2c46f58%2522%252C%2522user_unique_id%2522%253A%25227039226993380476457%2522%252C%2522timestamp%2522%253A1638947757142%257D; _gid=GA1.2.1579227660.1645168890; passport_csrf_token_default=031b6da32978211ece5a951645598b56; passport_csrf_token=031b6da32978211ece5a951645598b56; _tea_utm_cache_2018=undefined; passport_auth_status=9a61cc19feedb96bed131fe66ae12463%2C; passport_auth_status_ss=9a61cc19feedb96bed131fe66ae12463%2C; sid_guard=983d194fe7b1059e05410061f5a68d6f%7C1645168922%7C5184000%7CTue%2C+19-Apr-2022+07%3A22%3A02+GMT; uid_tt=cb8c23e83bf85a10a7388e2cc91f33d2; uid_tt_ss=cb8c23e83bf85a10a7388e2cc91f33d2; sid_tt=983d194fe7b1059e05410061f5a68d6f; sessionid=983d194fe7b1059e05410061f5a68d6f; sessionid_ss=983d194fe7b1059e05410061f5a68d6f; sid_ucp_v1=1.0.0-KDIyNDExZmFmNjQ4NTJhOGQ0ZjRmNjk1ZjI1YTdjOTQ2Nzg4YzVlOGEKFwjepLDLi4yRBRCakr2QBhiwFDgCQPEHGgJsZiIgOTgzZDE5NGZlN2IxMDU5ZTA1NDEwMDYxZjVhNjhkNmY; ssid_ucp_v1=1.0.0-KDIyNDExZmFmNjQ4NTJhOGQ0ZjRmNjk1ZjI1YTdjOTQ2Nzg4YzVlOGEKFwjepLDLi4yRBRCakr2QBhiwFDgCQPEHGgJsZiIgOTgzZDE5NGZlN2IxMDU5ZTA1NDEwMDYxZjVhNjhkNmY; n_mh=B3f8RxO6DuoYVWw4Xxz6K3t2yFmAj93jHHpIaYTFzoQ';
 
 /*************** 邮箱服务使用 ***************/
 // 发送方邮箱
-export const MAIL_SENDER = '';
+export const MAIL_SENDER = 'stevenrobot@yeah.net';
 // 发送方SMTP服务的授权码或者口令
-export const SMTP_PASSWORD = '';
+export const SMTP_PASSWORD = 'shouquan1617';
 // 发送邮件服务器
-export const MAIL_HOST_SENDER = '';
+export const MAIL_HOST_SENDER = 'smtp.yeah.net';
 // 发送邮件服务器的端口号
-export const MAIL_PORT_SENDER = '';
+export const MAIL_PORT_SENDER = '25';
 // 邮件接受方
-export const MAIL_RECIPIENT = '';
+export const MAIL_RECIPIENT = 'qcjy16@126.com';
 /*************** 邮箱服务使用 ***************/
